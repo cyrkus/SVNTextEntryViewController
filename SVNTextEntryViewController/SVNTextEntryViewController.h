c70cf3adb40292d3b92d44b3e391f2436cfda5f8
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-13 14:20:32 -0700

    Initial Commit

---
+++ SVNTextEntryViewController/SVNTextEntryViewController.h
@@ -0,0 +1,19 @@
+//
+//  SVNTextEntryViewController.h
+//  SVNTextEntryViewController
+//
+//  Created by Aaron Dean Bikis on 4/13/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNTextEntryViewController.
+FOUNDATION_EXPORT double SVNTextEntryViewControllerVersionNumber;
+
+//! Project version string for SVNTextEntryViewController.
+FOUNDATION_EXPORT const unsigned char SVNTextEntryViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNTextEntryViewController/PublicHeader.h>
+
+
