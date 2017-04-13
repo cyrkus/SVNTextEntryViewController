7360738a5ccc074ff59dc04d6678d6f05abd823f
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-13 14:36:51 -0700

    update styling

--- SVNTextEntryViewController/SVNTextEntryViewController.swift
+++ SVNTextEntryViewController/SVNTextEntryViewController.swift
@@ -27,7 +27,7 @@ open class SVNTextEntryViewController: SVNModalViewController {
             x: 15,
             y: self.dismissButton.frame.origin.y + self.dismissButton.frame.size.height + self.yPadding,
             width: self.view.frame.width - 15 * 2,
-            height: 65))
+            height: 55))
         label.textColor = self.theme.stanardButtonTintColor
         label.textAlignment = .center
         label.numberOfLines = 0
